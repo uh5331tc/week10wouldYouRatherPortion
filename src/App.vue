50a5baf38e129c657542dbc96daf1562dc99334f
Noelleb <email.email.com>
2022-04-20 18:50:48 -0500

    wouldyourather

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div id="app"> 
+
+  <h1> Would You Rather...</h1>
+
+  <would-you-rather v-bind:question="wyrQuestion" 
+  v-bind:answer1="wyrAnswer1"
+  v-bind:answer2="wyrAnswer2"
+  v-on:answer-changed="answerChanged"></would-you-rather>  <!--template has its own vueapp-->
+
+<p>{{ userSelectionMessage }} </p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WouldYouRather from './components/WouldYouRather.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WouldYouRather
+  },
+  data() {
+    return {
+      wyrQuestion: 'Would you rather be five years younger or five years older?',
+      wyrAnswer1: 'Younger',
+      wyrAnswer2: 'Older',
+      userSelectionMessage: ''
+    }
+  },
+  methods: {
+    answerChanged(choice) {
+      this.userSelectionMessage = `Prepare to be five years ${choice}!`
+    }
   }
 }
 </script>
 
 <style>
+
+body {
+  background: #fa4134;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #ffffff;
   margin-top: 60px;
+  background:#b30707;
 }
 </style>
---
+++ src/components/WouldYouRather.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="wyr">
+   
+   <h2><i>Please make your choice!</i></h2>
+   <h3>{{ question }}</h3>
+<br>
+   <input type="radio" v-model="choice" v-bind:value="answer1" v-on:change="choiceMade">
+   <label>{{ answer1 }} </label>
+
+   <input type="radio" v-model="choice" v-bind:value="answer2" v-on:change="choiceMade">
+   <label>{{ answer2 }} </label>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WouldYouRather',
+  props: {
+    question: String,
+    answer1: String,
+    answer2: String,
+  },
+  data() {
+    return {
+      choice: ''
+    }
+  }, 
+  methods: {
+    choiceMade() {
+      this.$emit('answer-changed', this.choice)
+     // console.log('choice!')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h2 {
+  background: rgb(214, 13, 13);
+  color: #000;
+}
+h3 {
+  border: 1px;
+  background:rgb(255, 58, 58);
+  margin: 40px 0 0;
+  color:rgb(7, 7, 7); 
+}
+
+
+.wyr { /*adding styles*/
+  margin: 10px;
+  border: 2px rgba(0, 0, 0, 0);
+  background: rgb(43, 44, 42);
+}
+
+</style>
